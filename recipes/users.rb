286c7859235f15f235f393385b488922a75bd5d7
Pirate Dunbar <yarr@piratedunbar.com>
2014-02-04 21:25:17 -0500

    Update users.rb

--- recipes/users.rb
+++ recipes/users.rb
@@ -8,10 +8,11 @@ if node[:db][:users]
 
 	node[:db][:users].each do |u|
 		name = ! u.include?('name') ? u : u['name']
+		passwd = ! u.include?('password') ? '' : u['password']
 
 		mysql_database_user name do
 			connection mysql_connection_info
-			password '' unless u['password']
+			password passwd
 			database_name u['db'] if u['db']
 			host '' unless u['host']
 			action [:grant, :create] unless u['action']
