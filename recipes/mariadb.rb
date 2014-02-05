acf75e62db08f3d82997d00e0c5e60eed23fe4da
Pirate Dunbar <yarr@piratedunbar.com>
2014-02-05 12:19:25 -0500

    Update mariadb.rb

--- recipes/mariadb.rb
+++ recipes/mariadb.rb
@@ -4,6 +4,3 @@ include_recipe 'mariadb::repo'
 include_recipe 'mysql::ruby'
 include_recipe 'mariadb::client'
 include_recipe 'mariadb::server'
-
-include_recipe 'db::databases'
-include_recipe 'db::users'
